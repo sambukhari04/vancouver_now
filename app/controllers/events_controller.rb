4330052cb8f1008efa5c23312d59f07b372ad7c9
Sam Bukhari <sambukhari04@gmail.com>
2017-11-14 20:02:03 -0800

    added category_id to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-
+  before_action :find_event, only: [:show, :edit#, :update,  :destroy]
+before_action :get_categories, only: [:new, :create#, :edit, :update]
 
   def new
     @event = Event.new
@@ -21,11 +22,17 @@ class EventsController < ApplicationController
   end
 
     def show
-      @event = Event.find params[:id]
+      # @event = Event.find params[:id]
     end
 
     def edit
-      @event = Event.find params[:id]
+      # @event = Event.find params[:id]
+    end
+
+    def update
+    end
+
+    def destroy
     end
 
     def index
@@ -45,7 +52,18 @@ class EventsController < ApplicationController
 
 
     private
+
     def event_params
         params.require(:event).permit(:title, :description, :price, :make_date, :location)
     end
-end
+
+    def find_event
+      @event = Event.find params[:id]
+    end
+
+
+    def get_categories
+      @categories = Category.all
+    end
+
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113035539) do
+ActiveRecord::Schema.define(version: 20171115033950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20171113035539) do
     t.date "make_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
+  add_foreign_key "events", "categories"
 end
