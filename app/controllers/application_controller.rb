c2b111e85876efc2e4d11fc5c7b0a448f5bef93e
Sam Bukhari <sambukhari04@gmail.com>
2017-12-04 16:41:09 -0800

    modal testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :initiate_instance_variables
 
   def user_signed_in?
     if session[:user_id].present? && current_user.nil?
@@ -9,11 +10,16 @@ class ApplicationController < ActionController::Base
   end
   helper_method :user_signed_in?
 
+  def initiate_instance_variables
+    @event1 = Event.new
+    @discount1 = Discount.new
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
   helper_method :current_user
-  
+
   private
   def authenticate_user!
     if !user_signed_in?
